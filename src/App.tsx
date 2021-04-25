288791bdc6f1c084649c45cdfd88903b3698bfa8
Maksim2806 <maksimpolezhaev@gmail.com>
2021-04-25 21:21:00 +0400

    added gh-pages build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 
 const App = () => {
   const [counter, setCounter] = useState<number>(0);
-  const a = 1;
 
   const handleClick = useCallback(() => {
     setCounter((count) => count + 1);
